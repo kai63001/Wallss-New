62f24005752075f989d3e3fb990133adef7ed5f7
kai63001 <animehq5@gmail.com>
2021-05-17 16:46:50 +0700

    update

--- clientSide/components/core/Input.tsx
+++ clientSide/components/core/Input.tsx
@@ -1,11 +1,21 @@
+import {useState} from 'react'
+
 interface inputData {
   name?: string;
   label?: string;
   error?: string;
   placeholder?: string;
+  password?: boolean;
 }
 
 const Input = (props: inputData) => {
+
+    const [password ,setPassword ] = useState(props.password ? true : false)
+
+    const showPwd = () => {
+        setPassword(!password)
+    }
+
   return (
     <div className="mb-3">
       <label
@@ -14,17 +24,24 @@ const Input = (props: inputData) => {
       >
         {props.label}
       </label>
-      <input
-        name={props.name || "input"}
-        id={props.name || "input"}
-        type="text"
-        className={` focus:ring-1  focus:outline-none w-full text-black placeholder-gray-500 border ${
-          props.error
-            ? "border-red-500 focus:border-red-500 focus:ring-red-500"
-            : "border-gray-200 focus:border-purple-500 focus:ring-purple-500"
-        } py-1 pl-3`}
-        placeholder={props.placeholder}
-      />
+      <div className="relative">
+        <input
+            name={props.name || "input"}
+            id={props.name || "input"}
+            type={password ? 'password' : 'text'}
+            className={` focus:ring-1  focus:outline-none w-full text-black placeholder-gray-500 border ${
+            props.error
+                ? "border-red-500 focus:border-red-500 focus:ring-red-500"
+                : "border-gray-200 focus:border-purple-500 focus:ring-purple-500"
+            } py-1 pl-3`}
+            placeholder={props.placeholder}
+        />
+        {props.password && (
+            <div onClick={showPwd} className="select-none absolute right-2 top-2.5 text-purple-600 cursor-pointer text-xs">
+                {password ? 'SHOW' : 'HIDE'}
+            </div>
+        )}
+      </div>
       {props.error && (
         <span className="text-sm text-red-500">{props.error}</span>
       )}
--- clientSide/pages/register.tsx
+++ clientSide/pages/register.tsx
@@ -5,29 +5,44 @@ const Input = dynamic(import("@/components/core/Input"));
 const Register = () => {
   const submit = (e) => {
     e.preventDefault();
-    console.log(e.target.romeo.value);
+    console.log(e.target.checkbox.checked);
   };
 
   return (
     <Layout>
       <div className="max-w-2xl mx-auto mt-14 px-2 sm:px-0">
-        <div className="bg-white p-11 shadow-md">
-          <h1 className="text-2xl font-romeo2">Create an Account</h1>
+        <div className="bg-white sm:p-11 p-5 shadow-md">
+          <h1 className="text-3xl font-romeo2">Create an Account</h1>
+          <span className="text-lg mt-1 block">
           Join the community.Share the things you create, or share the things
           you love
-          <br />
+          </span>
           <br />
           <form onSubmit={submit}>
-            <Input
-              name="name"
-              label="* Name"
-              placeholder="E.g. Jose Singh"
-            />
+            <Input name="name" label="* Name" placeholder="E.g. Jose Singh" />
             <Input
               name="username"
               label="* Username"
               placeholder="E.g. joseSingh"
             />
+            <Input
+              name="email"
+              label="* Email"
+              placeholder="E.g. jose@singh.com"
+            />
+            <Input
+              name="password"
+              label="* Password"
+              placeholder=""
+              password={true}
+            />
+            <div className="mb-2 block">
+              <input name="checkbox" className="checked:bg-purple-500" id="checkbox" type="checkbox" />
+              <label htmlFor="checkbox" className="ml-2 select-none">
+                By creating an account, you agree to our Terms of Service and
+                Privacy Policy.
+              </label>
+            </div>
             <input
               className="bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 text-white py-2 px-5 cursor-pointer"
               value="Create account"
