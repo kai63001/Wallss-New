97750c9ad87cbdb8085dff770cf2b5ef133b601d
kai63001 <animehq5@gmail.com>
2021-05-18 07:22:52 +0700

    add dropdown system

--- clientSide/components/Navbar/profile.tsx
+++ clientSide/components/Navbar/profile.tsx
@@ -1,27 +1,37 @@
 import Image from "next/image";
-import {useEffect} from 'react'
+import { useEffect, useState, useRef } from "react";
 const Profile = () => {
-    useEffect(() => {
-        // only add the event listener when the dropdown is opened
-        function handleClick(event) {
-          console.log('romeooo')
-        }
-        window.addEventListener("click", handleClick);
-        // clean up
-        return () => window.removeEventListener("click", handleClick);
-      });
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdown = useRef(null);
+  useEffect(() => {
+    if (!showDropdown) return;
+    function handleClick(event) {
+      if (dropdown.current && !dropdown.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    }
+    window.addEventListener("click", handleClick);
+    return () => window.removeEventListener("click", handleClick);
+  }, [showDropdown]);
   return (
-    <div className="flex justify-end h-full">
-      <Image
-        onClick={() => console.log("click")}
-        className="bg-purple-300 rounded-full cursor-pointer"
-        src={`https://avatarfiles.alphacoders.com/282/thumb-282572.png`}
-        alt="Picture of the author"
-        width={36}
-        height={36}
-        quality={100}
-        layout="intrinsic"
-      />
+    <div className="relative">
+      <div className="flex justify-end h-full">
+        <Image
+          onClick={() => setShowDropdown(b => !b)}
+          className="bg-purple-300 rounded-full cursor-pointer"
+          src={`https://avatarfiles.alphacoders.com/282/thumb-282572.png`}
+          alt="Picture of the author"
+          width={36}
+          height={36}
+          quality={100}
+          layout="intrinsic"
+        />
+      </div>
+      {showDropdown && (
+      <div ref={dropdown} className="absolute bg-white right-0 top-10 z-10 shadow">
+        <div className="p-3">asdasdas</div>
+      </div>
+      )}
     </div>
   );
 };
