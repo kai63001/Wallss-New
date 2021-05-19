83800cb1940514c77e867df07daf7e11e7d46961
kai63001 <animehq5@gmail.com>
2021-05-19 19:51:17 +0700

    server side cookie token check

--- clientSide/pages/upload.tsx
+++ clientSide/pages/upload.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import dynamic from "next/dynamic";
 const Layout = dynamic(import("@/components/Layout"));
 const Input = dynamic(import("@/components/core/Input"));
+import { isAuth } from '@/lib/auth'
 
-const UploadPage = () => {
+const UploadPage = (props) => {
+  // console.log(props.romeo)
   return (
     <Layout>
       <div className="max-w-2xl mx-auto mt-14 px-2 sm:px-0">
@@ -13,5 +15,33 @@ const UploadPage = () => {
     </Layout>
   );
 };
+export async function getServerSideProps({req,res}) {
+  const cookie = req ?  req.headers.cookie  : undefined
+  if(cookie == undefined){
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+      props:{},
+    };
+  }
+  //cookie parse
+  const token = cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1")
+  if(!isAuth(token)){
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+      props:{},
+    };
+  }
+  return {
+    props: {
+      // romeo: ctx.req ? { cookie: ctx.req.headers.cookie } : undefined
+    }, // will be passed to the page component as props
+  }
+}
 
 export default UploadPage;
