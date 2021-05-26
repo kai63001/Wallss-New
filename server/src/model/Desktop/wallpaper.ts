04bf590bd9772c95c674f30117d4b2df7ef13471
kai63001 <animehq5@gmail.com>
2021-05-26 17:39:37 +0700

    add model wallpaper

---
+++ server/src/model/Desktop/wallpaper.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const WallpaperSchema = new mongoose.Schema({
+    resolution: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        require: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    tags: [{
+        type: String,
+    }],
+    author: {
+        type: String
+    },
+    categoly: [{
+        type: String
+    }]
+})
+
+const Wallpaper = mongoose.model('Wallpaper', WallpaperSchema);
+
+export default Wallpaper;
