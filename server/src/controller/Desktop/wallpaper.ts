40ed4091ef872a1141818af324d90d42db3455c7
kai63001 <animehq5@gmail.com>
2021-05-27 17:00:04 +0700

    add limit

--- server/src/controller/Desktop/wallpaper.ts
+++ server/src/controller/Desktop/wallpaper.ts
@@ -1,10 +1,11 @@
 import express, { Router, Request, Response, NextFunction } from "express";
+import { WallpaperDesktop } from "../../model";
 
 const router: Router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
- 
+  const wall = await WallpaperDesktop.find({}).lean();
+  return res.send(wall);
 });
-
 
 export default router;
--- server/src/main.ts
+++ server/src/main.ts
@@ -15,7 +15,9 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({
+  limit: "15mb",
+}));
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
   console.log("asd");
