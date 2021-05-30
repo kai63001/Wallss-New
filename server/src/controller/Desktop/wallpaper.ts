d823e21662284addcb5dbb6a93004307d749bf8a
kai63001 <animehq5@gmail.com>
2021-05-30 17:13:22 +0700

    update desc

--- server/src/controller/Desktop/wallpaper.ts
+++ server/src/controller/Desktop/wallpaper.ts
@@ -4,7 +4,7 @@ import { WallpaperDesktop } from "../../model";
 const router: Router = express.Router();
 
 router.get("/index", async (req: Request, res: Response) => {
-  const wall = await WallpaperDesktop.find({}).select({image:1,name:1}).sort({_id: 1}).limit(12).lean();
+  const wall = await WallpaperDesktop.find({}).select({image:1,name:1}).sort({_id: -1}).limit(12).lean();
   return res.send(wall);
 });
 
