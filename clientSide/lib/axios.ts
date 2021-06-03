d47cfe2fca9e9ad4fec59c95c3336f16074499b7
kai63001 <animehq5@gmail.com>
2021-06-03 15:06:59 +0700

    chagen axios interceptor cookie

--- clientSide/lib/axios.ts
+++ clientSide/lib/axios.ts
@@ -1,8 +1,11 @@
 import axios from 'axios'
-import cookieCutter from 'cookie-cutter'
+// import cookieCutter from 'cookie-cutter'
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
 
 axios.interceptors.request.use((config) => {
-    const token = cookieCutter.get('token')
+    const token =  cookies.get('token') || ''
     config.headers.Authorization =  token;
 
     return config;
