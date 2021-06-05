0e8c39916e07346c68729c2c8a86fdf799e35f06
kai63001 <animehq5@gmail.com>
2021-06-06 00:37:01 +0700

    update

--- clientSide/components/core/Pagination.tsx
+++ clientSide/components/core/Pagination.tsx
@@ -1,27 +1,28 @@
 import ReactPaginate from "react-paginate";
 
-
 const Pagination = () => {
   return (
-    <div className="hidden sm:block">
-      <ReactPaginate
-        pageCount={1000}
-        pageRangeDisplayed={3}
-        marginPagesDisplayed={2}
-        containerClassName="flex items-center justify-center bg-white py-2 text-lg select-none"
-        pageClassName=" text-purple-800"
-        pageLinkClassName="px-2"
-        activeClassName="bg-purple-600 text-white"
-        activeLinkClassName="px-2 text-white"
-        previousClassName="text-purple-900"
-        nextClassName="text-purple-900"
-        previousLinkClassName="px-2"
-        nextLinkClassName="px-2"
-        breakLabel="···"
-        breakClassName="text-center text-purple-500"
-        breakLinkClassName="px-2"
-      />
-    </div>
+    <>
+      <div className="hidden sm:block text-purple">
+        <ReactPaginate
+          pageCount={1000}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          containerClassName="flex items-center justify-center bg-white py-3 text-lg select-none"
+          pageClassName="text-purple-700 hover:text-white hover:bg-purple-400 mx-1"
+          pageLinkClassName="px-2"
+          activeClassName="bg-purple-600 text-white px-1"
+          activeLinkClassName="px-2 text-white"
+          previousClassName="text-purple-900 hover:text-white hover:bg-purple-900 mx-1"
+          nextClassName="text-purple-900 hover:text-white hover:bg-purple-900 mx-1"
+          previousLinkClassName="px-2"
+          nextLinkClassName="px-2"
+          breakLabel="···"
+          breakClassName="text-center text-purple-500"
+          breakLinkClassName="px-2"
+        />
+      </div>
+    </>
   );
 };
 
