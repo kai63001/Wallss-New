a9678ffd9475109085318e46a02cb31c727a7d9a
kai63001 <animehq5@gmail.com>
2021-06-10 16:01:50 +0700

    add report

--- clientSide/pages/[type]/wall/[id].tsx
+++ clientSide/pages/[type]/wall/[id].tsx
@@ -101,6 +101,20 @@ const DesktopWallpaperPage = (props) => {
             </div>
           </div>
           <div className="flex items-center mb-1">
+            <button className="text-yellow-500 bg-yellow-100 px-5 py-2.5 flex w-full sm:w-auto items-center justify-center mr-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
             <button
               // href={`/_next/image?url=${encodeURIComponent(
               //   props.data?.image?.replace(/thumb-1920-/g, "")
