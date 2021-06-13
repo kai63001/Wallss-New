e887345c579ded87b17179fbd25e7cf92d77a5a8
kai63001 <animehq5@gmail.com>
2021-06-13 19:21:12 +0700

    📃pagination model and download model

--- server/src/model/Desktop/wallpaper.ts
+++ server/src/model/Desktop/wallpaper.ts
@@ -1,57 +1,70 @@
-import mongoose from 'mongoose';
-import mongoosePaginate from 'mongoose-paginate-v2'
+import mongoose, { PaginateModel, Document, Schema } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 
-interface IAccessStatsSchema {
-    type: string;
-    views: number;
-    paginate: ()=>{};
-  }
+interface IAccessStatsSchema extends Document {
+  download: number;
+}
 const WallpaperSchema = new mongoose.Schema({
-    resolution: {
-        type: String,
+  resolution: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    require: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  tags: [
+    {
+      type: String,
+      lowercase: true,
+      index: true,
+      trim: true,
     },
-    name: {
-        type: String,
+  ],
+  author: {
+    type: String,
+  },
+  authorLink: {
+    type: String,
+  },
+  categoly: [
+    {
+      type: String,
+      lowercase: true,
+      index: true,
+      trim: true,
     },
-    image: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        require: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    tags: [{
-        type: String,
-        lowercase: true,
-        index: true,
-        trim: true
-    }],
-    author: {
-        type: String
-    },
-    authorLink: {
-        type: String
-    },
-    categoly: [{
-        type: String,
-        lowercase: true,
-        index: true,
-        trim: true
-    }],
-    type : {
-        type: Number,
-        index: true,
-        default: 0
-    }
-})
+  ],
+  type: {
+    type: Number,
+    index: true,
+    default: 0,
+  },
+  download: {
+    type: Number,
+    default: 0,
+  },
+});
 
-WallpaperSchema.plugin(mongoosePaginate)
+WallpaperSchema.plugin(mongoosePaginate);
 
-const Wallpaper = mongoose.model('Wallpaper', WallpaperSchema);
+//  extend PaginateModel
+interface WallpaperModel<T extends Document> extends PaginateModel<T> {}
+
+const Wallpaper: WallpaperModel<IAccessStatsSchema> =
+  mongoose.model<IAccessStatsSchema>(
+    "Wallpaper",
+    WallpaperSchema
+  ) as WallpaperModel<IAccessStatsSchema>;
 
 export default Wallpaper;
