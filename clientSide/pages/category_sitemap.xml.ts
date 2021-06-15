fc0b1cd0d5e0fd05d72b2af9b86a5c917f06a735
kai63001 <animehq5@gmail.com>
2021-06-15 20:51:44 +0700

    add category sitemap 🗾

---
+++ clientSide/pages/category_sitemap.xml.ts
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = ({ res, req }) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"> 
+  <url>
+    <loc>https://${req.headers.host}/sitemap.xml?page=1</loc>
+  </url>
+</urlset>
+`.trim();
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
