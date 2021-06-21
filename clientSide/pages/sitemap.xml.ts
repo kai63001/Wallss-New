cba16231bd093ad9bb35bbf0c40178582f22d90e
kai63001 <animehq5@gmail.com>
2021-06-21 17:28:28 +0700

    🥙 sitemap main done

--- clientSide/pages/sitemap.xml.ts
+++ clientSide/pages/sitemap.xml.ts
@@ -1,13 +1,26 @@
+import axios from "axios";
 import React from "react";
 
 const Sitemap = () => {};
 
-export const getServerSideProps = ({ res, req }) => {
+export const getServerSideProps = async ({ res, req }) => {
+  const data = await (await axios.get(`${process.env.HOST}/sitemap`)).data;
+  // console.log(data);
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-    <loc>https://${req.headers.host}/sitemap.xml?page=1</loc>
-  </sitemap>
+${[...Array(data?.wallpaper)].map(
+  (data, i) =>
+    `<sitemap>
+<loc>https://${req.headers.host}/wallpaper_sitemap.xml?page=${i + 1}</loc>
+</sitemap>`
+)}
+${[...Array(data?.category)].map(
+  (data, i) =>
+    `<sitemap>
+<loc>https://${req.headers.host}/category_sitemap.xml?page=${i + 1}</loc>
+</sitemap>`
+)}
+  
 </sitemapindex>
 `.trim();
 
--- server/src/controller/sitemap/main.ts
+++ server/src/controller/sitemap/main.ts
@@ -1,12 +1,18 @@
 import express, { Router, Request, Response, NextFunction } from "express";
-import { Category } from "../../model";
+import { Category, WallpaperDesktop } from "../../model";
 import mongoose from "mongoose";
 var fs = require("fs");
 const router: Router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  
-  res.send("fuck");
+  const wallpaperCount = await WallpaperDesktop.countDocuments({});
+  const categoryCount = await Category.countDocuments({});
+  const body = {
+    wallpaper: Math.ceil(wallpaperCount / 10000),
+    category: Math.ceil(categoryCount / 10000),
+  };
+  console.log(wallpaperCount);
+  res.json(body);
 });
 
 export default router;
